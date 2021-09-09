80a3f6bf0a3f86216c59cdb12d9104c2ac2f9d17
molko-did-a-nerd-stuff <0824molo@gmail.com>
2021-09-09 13:02:19 +0800

    why tf the commits arent working

--- slice/slice.hpp
+++ slice/slice.hpp
@@ -14,27 +14,27 @@ struct slice {
     public:
     constexpr slice() : ptr(nullptr), _size(0) {}
     template <size_t L>
-    ///Creates slice from a std::array
+    ///creates slice from a std::array
     ///@param arr array
     constexpr slice(const std::array<T, L> &arr) : ptr((T*)arr.data()), _size(L) {}
-    ///Creates slice from a std::vector
+    ///creates slice from a std::vector
     ///@param vec vector
     constexpr slice(const std::vector<T> &vec) : ptr((T*)vec.data()), _size(vec.size()) {}
-    ///Creates slice from a pointer and a length
+    ///creates slice from a pointer and a length
     ///@param ptr pointer
     ///@param size size
     constexpr slice(T *ptr, size_t size) : ptr(ptr), _size(size) {}
 
-    ///Gets element in the given index
-    ///Wont panic when index out of range which could cause segfault
-    ///Use at() if you want more safer method
+    ///gets element in the given index
+    ///wont panic when index out of range which could cause segfault
+    ///use at() if you want more safer method
     ///@param i index
     ///@return immutable reference to the element(for mutable reference, get from mutable slice)
     constexpr inline const T &operator[](size_t i) const {
         return ptr[i];
     }
-    ///Gets element in the given index
-    ///It will panic when index out of range
+    ///gets element in the given index
+    ///it will panic when index out of range
     ///@param i index
     ///@return immutable reference to the element(for mutable reference, get from mutable slice)
     constexpr const T &at(size_t i) const {
@@ -51,16 +51,16 @@ struct slice {
 
         return ptr[i];
     }
-    ///Gets element in the given index
-    ///Wont panic when index out of range which could cause segfault
-    ///Use at() if you want more safer method
+    ///gets element in the given index
+    ///wont panic when index out of range which could cause segfault
+    ///use at() if you want more safer method
     ///@param i index
     ///@return mutable reference to the element(for immutable reference, get from immutable slice)
     constexpr inline T &operator[](size_t i) {
         return ptr[i];
     }
-    ///Gets element in the given index
-    ///It will panic when index out of range
+    ///gets element in the given index
+    ///it will panic when index out of range
     ///@param i index
     ///@return mutable reference to the element(for immutable reference, get from immutable slice)
     constexpr T &at(size_t i) {
@@ -77,9 +77,9 @@ struct slice {
 
         return ptr[i];
     }
-    ///Returns size
+    ///@return size of the slice
     constexpr inline size_t size() const {return _size;}
-    ///Returns immutable pointer
+    ///@return size of the slice
     constexpr inline const T *data() const {return ptr;}
 
     constexpr inline T *begin() {return ptr;}
